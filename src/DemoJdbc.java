3b806bbb3a0b6a221ef7d011de4f74831b160391
webmaster <2021wa86615@wilp.bits-pilani.ac.in>
2025-10-20 10:38:01 +0530

    Banking System Demo Programme

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -9,12 +9,11 @@ public class DemoJdbc {
 5. Create Statement: Create statement object for SQL execution
 6. Execute Statement: Execute SQL queries/updates
 7. Close Connection: Release database resources */
-        String url = "jdbc:postgresql://localhost:5432/postgres";  // <-- database name required
+        String url = "jdbc:postgresql://localhost:5432/bankingdb";  // <-- database name required
         String uname = "postgres";   // your postgres username
         String pass = "6137";  // your postgres password
-
-       // Class.forName("org.postgresql.Driver");
-        Connection con= DriverManager.getConnection(url,uname,pass);
+        Class.forName("org.postgresql.Driver");
+        Connection con =DriverManager.getConnection(url,uname,pass);
         System.out.println("Connected to database successfully");
         con.close();
     }
